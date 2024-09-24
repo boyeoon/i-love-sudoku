847b3fdf66ae7c360fd09af836dd622c42c0b9b6
BOYEON JANG <boyeonjang@BOYEONui-MacBookAir.local>
2024-09-25 03:14:46 +0900

    chore: update button component

--- app/_components/Button.tsx
+++ app/_components/Button.tsx
@@ -4,12 +4,16 @@ import React from "react";
 
 interface ButtonProps {
   buttonName: string;
+  onClick: () => void;
+  className?: string;
 }
 
-export default function Button({ buttonName }: ButtonProps) {
+export default function Button({ buttonName, onClick }: ButtonProps) {
   return (
     <div className="bg-[#255AB4] flex justify-center px-4 py-2.5 rounded-md hover:bg-[#255AB4]/75">
-      <button>{buttonName}</button>
+      <button onClick={onClick}>
+        {buttonName}
+      </button>
     </div>
   );
 }
