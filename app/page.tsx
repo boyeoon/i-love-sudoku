4200a5a4b3807262c581ab43e0b8890aec44406b
BOYEON JANG <boyeonjang@BOYEONui-MacBookAir.local>
2024-09-25 03:43:07 +0900

    fix: delete unnecessary div tags

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
 import Image from "next/image";
+import Sudoku from "./_components/Sudoku";
+import DropdownButton from "./_components/dropdownButton"
+import Button from "./_components/Button"
 
 export default function Home() {
   return (
@@ -7,6 +10,7 @@ export default function Home() {
         <div className="">
           <p className="text-2xl font-bold">I LOVE SUDOKU</p>
         </div>
+        <button className="bg-[#255AB4] px-4 py-2.5 rounded-md">Level</button>
         <button className="bg-[#255AB4] px-4 py-2.5 rounded-md">Language</button>
       </header>
       <main className="px-24">
@@ -15,36 +19,18 @@ export default function Home() {
           <a>Easy 01</a>
           <button className="bg-[#255AB4] px-4 py-2.5 rounded-md">Reset</button>
         </div>
+        {/* <DropdownButton menuName="내 메뉴" options={['옵션 1', '옵션 2', '옵션 3']} /> */}
         <div className="flex justify-between">
-          {/* left */}
-          <div className="bg-[#ffffff] w-[37.5rem] h-[37.5rem] rounded-md">
-            <div>
-
-            </div>
-          </div>
-          <div className="w-16">
-
-          </div>
-          {/* right */}
-          <div className="content-between grid w-80">
-            {/* button */}
-            <div className="grid grid-cols-3 gap-2">
-              <button className="bg-[#255AB4] px-4 py-2.5 rounded-md">1</button>
-              <button className="bg-[#255AB4] px-4 py-2.5 rounded-md">2</button>
-              <button className="bg-[#255AB4] px-4 py-2.5 rounded-md">3</button>
-              <button className="bg-[#255AB4] px-4 py-2.5 rounded-md">4</button>
-              <button className="bg-[#255AB4] px-4 py-2.5 rounded-md">5</button>
-              <button className="bg-[#255AB4] px-4 py-2.5 rounded-md">6</button>
-              <button className="bg-[#255AB4] px-4 py-2.5 rounded-md">7</button>
-              <button className="bg-[#255AB4] px-4 py-2.5 rounded-md">8</button>
-              <button className="bg-[#255AB4] px-4 py-2.5 rounded-md">9</button>
-            </div>
-            {/* description */}
-            <p>
-              <span className="block text-2xl font-bold">About</span>
-              <span>This app is one of my side projects. You can view the source code here on <a className="font-bold underline underline-offset-1" href="https://github.com/BoYeonJang/i-love-sudoku" target="_blank">GitHub</a>. Created by Boyeon Jang</span>
-            </p>
-          </div>
+        <Sudoku />
+        <div className="w-16" />
+        <div className="content-between grid w-80">
+          {/* button */}
+          {/* description */}
+          <p>
+            <span className="block text-2xl font-bold">About</span>
+            <span>This app is one of my side projects. You can view the source code here on <a className="font-bold underline underline-offset-1" href="https://github.com/BoYeonJang/i-love-sudoku" target="_blank">GitHub</a>. Created by Boyeon Jang</span>
+          </p>
+        </div>
         </div>
       </main>
       <footer className="px-24 py-8">
