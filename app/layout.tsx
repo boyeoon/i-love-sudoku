4b56b6fa6a1265878555f94894c021d903ff25d7
Boyeon Jang <uko010199@gmail.com>
2024-10-16 04:12:18 +0900

    chore: remove local fonts and use web fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const pretendard = localFont({
-  src: './fonts/PretendardVariable.woff2',
-  display: 'swap',
-  weight: '45 920',
-})
-
 export const metadata: Metadata = {
   title: "I LOVE SUDOKU",
   description: "My Sudoku Side Project",
@@ -21,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${pretendard.className} antialiased`}
+        // className={`${intelone.className} antialiased`}
       >
         {children}
       </body>
