06e89717a382d3852bb2073b4ffc29b60ec84469
Boyeon Jang <uko010199@gmail.com>
2024-11-11 02:05:10 +0900

    style: udpate margin values

--- app/components/modal.tsx
+++ app/components/modal.tsx
@@ -1,4 +1,4 @@
-import Button from "@/components/button";
+import Button from "@/components/buttons/button";
 
 interface ModalProps {
   isVisible: boolean;
@@ -11,7 +11,7 @@ export default function Modal({ isVisible, onClose }: ModalProps) {
   return (
     <div>
       <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-8 z-50 text-center rounded-md">
-        <h2 className="text-xl font-bold text-[#255AB4]">Success</h2>
+        <h2 className="text-xl font-bold text-[#255AB4] mb-4">Success!</h2>
         <Button onClick={onClose} buttonName="New game" />
       </div>
       <div className="fixed inset-0 bg-black opacity-50 z-40" />
