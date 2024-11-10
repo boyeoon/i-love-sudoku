d6890c516549d73a68d41beb93e46e0189ba06e6
Boyeon Jang <uko010199@gmail.com>
2024-11-10 22:23:42 +0900

    feat: add header

---
+++ app/components/header.tsx
@@ -0,0 +1,13 @@
+// import Button from "./_components/Button"
+
+export default function Header() {
+  return (
+    <header className="bg-[#1D1E27] flex items-center justify-between px-24 py-8 w-full">
+      <div className="flex-grow">
+        <p className="text-2xl font-bold">I LOVE SUDOKU</p>
+      </div>
+      {/* <button className="bg-[#255AB4] px-4 py-2.5 rounded-md">Level</button> */}
+      {/* <button className="bg-[#255AB4] px-4 py-2.5 rounded-md">Language</button> */}
+    </header>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,12 @@
-// import Image from "next/image";
-import Sudoku from "./components/Sudoku";
+import Sudoku from "@/components/sudoku";
+import Header from "@/components/header";
 // import DropdownButton from "./_components/dropdownButton"
 // import Button from "./_components/Button"
 
 export default function Home() {
   return (
     <div>
-      <header className="bg-[#1D1E27] flex items-center justify-between px-4 py-8 md:px-24 w-full">
-        <div className="flex-grow">
-          <p className="text-2xl font-bold">I LOVE SUDOKU</p>
-        </div>
-        {/* <button className="bg-[#255AB4] px-4 py-2.5 rounded-md">Level</button> */}
-        {/* <button className="bg-[#255AB4] px-4 py-2.5 rounded-md">Language</button> */}
-      </header>
+      <Header />
       <main className="flex justify-center my-10">
         {/* top */}
         {/* <div className="flex items-end justify-between my-3.5">
@@ -20,7 +14,7 @@ export default function Home() {
           <button className="bg-[#255AB4] px-4 py-2.5 rounded-md">Reset</button>
         </div> */}
         {/* <DropdownButton menuName="내 메뉴" options={['옵션 1', '옵션 2', '옵션 3']} /> */}
-        <div className="">
+        <div>
           <Sudoku />
         </div>
       </main>
